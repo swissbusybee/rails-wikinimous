009c5ac9a9e86124135b1731d7f02a6bcb432726
Sue Rocha <swissbusybee@gmail.com>
2020-04-15 15:50:49 +0200

    Created Articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Article.destroy_all
+
+def fake_content
+  paragraphs = []
+  paragraphs << Faker::Lorem.paragraph
+  paragraphs << "## #{Faker::Lorem.sentence}"
+  paragraphs << Faker::Lorem.paragraphs(3)
+  paragraphs << "**#{Faker::Lorem.word}** #{Faker::Lorem.sentences.join}"
+  paragraphs.flatten.join("\n\n")
+end
+
+20.times do
+  Article.create(
+    title: Faker::Lorem.sentence,
+    content: fake_content
+  )
+end
